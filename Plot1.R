73b6ae5be6a7bfc610f1e716367ec9e22eb4fc67
dB <73113072+dbaca8@users.noreply.github.com>
2021-01-08 22:54:28 -0800

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,101 @@
+### Exploratory Data Analysis 
+## Week 1 Project
+## Plot 1
+
+## 1. Download file household_power_consumption.txt
+## 2. Subset the data taken over 2 days: 2007-02-01 and 2007-02-02
+## 3. Create histogram plot of Global_active_power (kilowatts) vs Frequency
+
+library(lubridate)
+
+## Directory commands
+getwd()
+setwd("~/Desktop") 
+
+## Checking and Create Directory "data" if one does not exist
+if(!file.exists("data")){
+        dir.create("data")
+}
+
+## Download Dataset.zip from Web
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip "
+zipfilepath <- "./data/exdata_data_household_power_consumption.zip"
+download.file(fileUrl, destfile = zipfilepath) 
+
+
+## unzip Data Set
+unzip(zipfilepath, exdir="~/Desktop/data")
+
+list.files("~/Desktop/data")
+# [1] "exdata_data_household_power_consumption.zip" "household_power_consumption.txt" 
+
+filepath <- "~/Desktop/data/household_power_consumption.txt"
+
+## Record date of downloaded file
+dateDownloaded <- date()
+dateDownloaded
+# [1] "Fri Jan  8 20:04:32 2021"
+
+## Reading Data Frame
+data <- read.table(filepath, sep=";", header=TRUE, stringsAsFactors=FALSE)
+str(data)
+# 'data.frame':	2075259 obs. of  9 variables:
+# $ Date                 : chr  "16/12/2006" "16/12/2006" "16/12/2006" "16/12/2006" ...
+# $ Time                 : chr  "17:24:00" "17:25:00" "17:26:00" "17:27:00" ...
+# $ Global_active_power  : chr  "4.216" "5.360" "5.374" "5.388" ...
+# $ Global_reactive_power: chr  "0.418" "0.436" "0.498" "0.502" ...
+# $ Voltage              : chr  "234.840" "233.630" "233.290" "233.740" ...
+# $ Global_intensity     : chr  "18.400" "23.000" "23.000" "23.000" ...
+# $ Sub_metering_1       : chr  "0.000" "0.000" "0.000" "0.000" ...
+# $ Sub_metering_2       : chr  "1.000" "1.000" "2.000" "1.000" ...
+# $ Sub_metering_3       : num  17 16 17 17 17 17 17 17 17 16 ...
+
+dim(data)
+# [1] 2075259     9
+
+## Convert classes of all columns to correct class
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+data$Time <- format(data$Time, format="%H:%M:%S")
+data$Global_active_power <- as.numeric(data$Global_active_power)
+data$Global_reactive_power <- as.numeric(data$Global_reactive_power)
+data$Voltage <- as.numeric(data$Voltage)
+data$Global_intensity <- as.numeric(data$Global_intensity)
+data$Sub_metering_1 <- as.numeric(data$Sub_metering_1)
+data$Sub_metering_2 <- as.numeric(data$Sub_metering_2)
+
+str(data)
+# 'data.frame':	2075259 obs. of  9 variables:
+# $ Date                 : Date, format: "2006-12-16" "2006-12-16" "2006-12-16" ...
+# $ Time                 : chr  "17:24:00" "17:25:00" "17:26:00" "17:27:00" .....
+# $ Global_active_power  : num  4.22 5.36 5.37 5.39 3.67 ...
+# $ Global_reactive_power: num  0.418 0.436 0.498 0.502 0.528 0.522 0.52 0.52 0.51 0.51 ...
+# $ Voltage              : num  235 234 233 234 236 ...
+# $ Global_intensity     : num  18.4 23 23 23 15.8 15 15.8 15.8 15.8 15.8 ...
+# $ Sub_metering_1       : num  0 0 0 0 0 0 0 0 0 0 ...
+# $ Sub_metering_2       : num  1 1 2 1 1 2 1 1 1 2 ...
+# $ Sub_metering_3       : num  17 16 17 17 17 17 17 17 17 16 ...
+
+dim(data)
+# > dim(data)
+# [1] 2075259       9
+
+## Subset data from 2007-02-01 and 2007-02-02
+datasub <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02")
+str(datasub)
+# 'data.frame':	2880 obs. of  9 variables:
+# $ Date                 : Date, format: "2007-02-01" "2007-02-01" "2007-02-01" ...
+# $ Time                 : chr  "00:00:00" "00:01:00" "00:02:00" "00:03:00" ...
+# $ Global_active_power  : num  0.326 0.326 0.324 0.324 0.322 0.32 0.32 0.32 0.32 0.236 ...
+# $ Global_reactive_power: num  0.128 0.13 0.132 0.134 0.13 0.126 0.126 0.126 0.128 0 ...
+# $ Voltage              : num  243 243 244 244 243 ...
+# $ Global_intensity     : num  1.4 1.4 1.4 1.4 1.4 1.4 1.4 1.4 1.4 1 ...
+# $ Sub_metering_1       : num  0 0 0 0 0 0 0 0 0 0 ...
+# $ Sub_metering_2       : num  0 0 0 0 0 0 0 0 0 0 ...
+# $ Sub_metering_3       : num  0 0 0 0 0 0 0 0 0 0 ...
+dim(datasub)
+# [1] 2880    9
+
+## Plot png histogram of Global_active_power vs Frequency for those 2 days
+png(file="plot1.png", width=480, height=480)
+hist(datasub$Global_active_power, col="red", border="black", main ="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
+dev.off()
---
+++ Plot4.R
@@ -0,0 +1,99 @@
+### Exploratory Data Analysis 
+## Week 1 Project
+## Plot 4
+
+## 1. Download file household_power_consumption.txt
+## 2. Subset the data taken over 2 days: 2007-02-01 and 2007-02-02
+## 3. Create 4 plots on 1 page: Global_active_power vs. time, Voltage vs. time,
+##    all Sub_metering vs. time, and Global_reactive_power vs. time
+
+library(lubridate)
+
+## Directory commands
+getwd()
+setwd("~/Desktop") 
+
+## Checking and Create Directory "data" if one does not exist
+if(!file.exists("data")){
+        dir.create("data")
+}
+
+## Download Dataset.zip from Web
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip "
+zipfilepath <- "./data/exdata_data_household_power_consumption.zip"
+download.file(fileUrl, destfile = zipfilepath) 
+
+
+## unzip Data Set
+unzip(zipfilepath, exdir="~/Desktop/data")
+
+list.files("~/Desktop/data")
+# [1] "exdata_data_household_power_consumption.zip" "household_power_consumption.txt" 
+
+filepath <- "~/Desktop/data/household_power_consumption.txt"
+
+## Record date of downloaded file
+dateDownloaded <- date()
+dateDownloaded
+# [1] "Fri Jan  8 21:10:44 2021"
+
+## Reading Data Frame
+data <- read.table(filepath, sep=";", header=TRUE, stringsAsFactors=FALSE)
+str(data)
+# 'data.frame':	2075259 obs. of  9 variables:
+# $ Date                 : chr  "16/12/2006" "16/12/2006" "16/12/2006" "16/12/2006" ...
+# $ Time                 : chr  "17:24:00" "17:25:00" "17:26:00" "17:27:00" ...
+# $ Global_active_power  : chr  "4.216" "5.360" "5.374" "5.388" ...
+# $ Global_reactive_power: chr  "0.418" "0.436" "0.498" "0.502" ...
+# $ Voltage              : chr  "234.840" "233.630" "233.290" "233.740" ...
+# $ Global_intensity     : chr  "18.400" "23.000" "23.000" "23.000" ...
+# $ Sub_metering_1       : chr  "0.000" "0.000" "0.000" "0.000" ...
+# $ Sub_metering_2       : chr  "1.000" "1.000" "2.000" "1.000" ...
+# $ Sub_metering_3       : num  17 16 17 17 17 17 17 17 17 16 ...
+
+dim(data)
+# [1] 2075259     9
+
+## Create column in table with date and time merged together
+Date_Time <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+data <- cbind(data, Date_Time)
+
+## Convert classes of all columns to correct class
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+data$Time <- format(data$Time, format="%H:%M:%S")
+data$Global_active_power <- as.numeric(data$Global_active_power)
+data$Global_reactive_power <- as.numeric(data$Global_reactive_power)
+data$Voltage <- as.numeric(data$Voltage)
+data$Global_intensity <- as.numeric(data$Global_intensity)
+data$Sub_metering_1 <- as.numeric(data$Sub_metering_1)
+data$Sub_metering_2 <- as.numeric(data$Sub_metering_2)
+
+### Subset data from 2007-02-01 and 2007-02-02
+datasub <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02")
+str(datasub)
+# 'data.frame':	2880 obs. of  10 variables:
+# $ Date                 : Date, format: "2007-02-01" "2007-02-01" "2007-02-01" ...
+# $ Time                 : chr  "00:00:00" "00:01:00" "00:02:00" "00:03:00" ...
+# $ Global_active_power  : num  0.326 0.326 0.324 0.324 0.322 0.32 0.32 0.32 0.32 0.236 ...
+# $ Global_reactive_power: num  0.128 0.13 0.132 0.134 0.13 0.126 0.126 0.126 0.128 0 ...
+# $ Voltage              : num  243 243 244 244 243 ...
+# $ Global_intensity     : num  1.4 1.4 1.4 1.4 1.4 1.4 1.4 1.4 1.4 1 ...
+# $ Sub_metering_1       : num  0 0 0 0 0 0 0 0 0 0 ...
+# $ Sub_metering_2       : num  0 0 0 0 0 0 0 0 0 0 ...
+# $ Sub_metering_3       : num  0 0 0 0 0 0 0 0 0 0 ...
+# $ Date_Time            : POSIXct, format: "2007-02-01 00:00:00" "2007-02-01 00:01:00" "2007-02-01 00:02:00" ...
+dim(datasub)
+# [1] 2880   10
+
+## 4 plots on 1 page: Global_active_power vs. time, Voltage vs. time,
+##    all Sub_metering vs. time, and Global_reactive_power vs. time
+png("plot4.png", width=480, height=480)
+par(mfrow=c(2,2))
+with(datasub, plot(Date_Time, Global_active_power, type="l", xlab="", ylab="Global Active Power"))
+with(datasub, plot(Date_Time, Voltage, type = "l", xlab="datetime", ylab="Voltage"))
+with(datasub, plot(Date_Time, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering"))
+lines(datasub$Date_Time, datasub$Sub_metering_2,type="l", col= "red")
+lines(datasub$Date_Time, datasub$Sub_metering_3,type="l", col= "blue")
+legend(c("topright"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty= 1, lwd=1, col = c("black", "red", "blue"))
+with(datasub, plot(Date_Time, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power"))
+dev.off()
